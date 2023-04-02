1cb3607605876a0e94417627d91def7b4643c994
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2023-04-03 09:43:02 +1200

    Update header.php

--- header.php
+++ header.php
@@ -1,30 +1,16 @@
-<!-- Start of Navbar -->
-<div class=".d-md-none .d-lg-block" style="background-color: rgba(154, 38, 38, 0);">
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="login.php">Bison Steakhouse</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Kanye West</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="menu.php">Menu</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="aboutus.php">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contactus.php">Contact Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="gallery.php">Gallery</a>
-        </li>
-      </ul>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
     </div>
   </div>
 </nav>
-</div>
