d2d5c6916a7c8105c342f742d67bcd5b5abbbf23
BilalKhanRongotai <bilal.khan@my.rongotai.school.nz>
2023-04-05 12:26:42 +1200

    jnjnjnj

--- includes/header.php
+++ includes/header.php
@@ -25,7 +25,6 @@
   
   <a href="#"><img src="image/logo.png" style="width:70px;height:80px;" alt="home"></a>
 
-   <!-- <a class="navbar-brand" href="#">Kanye West</a>-->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
