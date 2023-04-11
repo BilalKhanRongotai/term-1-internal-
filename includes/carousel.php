f612346321bc03f0e866512be3336ced888cd513
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2023-04-11 20:45:05 +1200

    cards on home page

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,34 +1,34 @@
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="3000">
-      <img src="image/Carousel1.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel1.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel2.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel2.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel3.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel3.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel4.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel4.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel5.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel5.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel6.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel6.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel7.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel7.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel8.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel8.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel9.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel9.jpg" class="d-block w-100" alt="Image could not load">
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src="image/Carousel10.jpg" class="d-block w-100" alt="...">
+      <img src="image/Carousel10.jpg" class="d-block w-100" alt="Image could not load">
     </div>
   </div>
   <button class="carousel-control-prev reverse-colour" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
--- index.php
+++ index.php
@@ -6,26 +6,53 @@ include 'includes/nav.php';
 include 'includes/carousel.php';
 ?>
 <br>
-<h2 class="white centered underlined">The website for all things Kanye West</h2>
+<h2 class="white centered underlined">The website for all things Ye</h2>
 
 <br>
-<div class="card card-left" style="width: 40rem;">
-  <img src="image/card1.webp" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card card-left">
+      <img src="image/card1.webp" class="card-img-top" alt="Image could not load">
+      <div class="card-body">
+        <h5 class="card-title">Hip Hop</h5>
+        <p class="card-text">Kanye West has an amazing and extensive discography of groundbreaking mucsic. Over the years Ye has released many influencial albums that have changed the hip hop industry forever. Here you can see all of his albums and songs.</p>
+        <a href="#" class="btn btn-dark">Music</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card-right">
+      <img src="image/card2.jpg" class="card-img-top" alt="Image could not load">
+      <div class="card-body">
+        <h5 class="card-title">Yeezy</h5>
+        <p class="card-text">Founded by Kanye West, clothing brand Yeezy is known for its minimalist and futuristic designs. In collabaration with adidas, Yeezys footwear and assortment of other items has become very popular. Here you can see some of yeezys best, most popular colthes.</p>
+        <a href="#" class="btn btn-dark">Fashion</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card-left">
+      <img src="image/card3.jpg" class="card-img-top" alt="Image could not load">
+      <div class="card-body">
+        <h5 class="card-title">The life of Kanye</h5>
+        <p class="card-text">Across the years Kanye west has lived a very interesting and exiting life. Throughout the years he has done alot of very important things. You can look here to learn more about the life of Ye, the artist formerly known as Kanye West.</p>
+        <a href="#" class="btn btn-dark">History</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card card-right">
+      <img src="image/card4.jpg" class="card-img-top" alt="Image could not load">
+      <div class="card-body">
+        <h5 class="card-title">Images</h5>
+        <p class="card-text">With a career as prolific as Ye's there will be a copious amount of photos, images, and art. We have gone ahead and compiled it all into one gallery so that you can take a look at it yourself.</p>
+        <a href="#" class="btn btn-dark">Gallery</a>
+      </div>
+    </div>
   </div>
 </div>
-<br>
-<div class="card card-right" style="width: 40rem;">
-  <img src="image/card1.webp" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+
+
 
 <!-- Call up the footer-->
 <?php
