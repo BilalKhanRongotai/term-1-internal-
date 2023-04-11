a06d4ee00b76b4fd9bf91bbf5b5d763cac0b809c
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2023-04-11 18:17:18 +1200

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,25 @@ include 'includes/carousel.php';
 <br>
 <h2 class="white centered underlined">The website for all things Kanye West</h2>
 
+<br>
+<div class="card card-left" style="width: 40rem;">
+  <img src="image/card1.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<br>
+<div class="card card-right" style="width: 40rem;">
+  <img src="image/card1.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
 <!-- Call up the footer-->
 <?php
 include 'includes/footer.php';
