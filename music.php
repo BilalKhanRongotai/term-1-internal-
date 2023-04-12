6d1956cebd44033deb7996db3232de8a4860d572
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2023-04-12 16:22:07 +1200

    Albums on music page

--- music.php
+++ music.php
@@ -5,21 +5,66 @@ include 'includes/header.php';
 include 'includes/nav.php';
 ?>
 
-<div">
 <h2 class="white underlined margin-left">Albums:</h2>
-</div>
+
 <br>
-<div class="container text-center">
+<div class="container" style="max-width:93%;">
   <div class="row">
-    <div class="col">
-    <img src="image/card1.webp" class="card-img-top" alt="Image could not load">
+    <div class="col grow">
+    <img src="image/TCDcover.webp" role="button" href="#" class="img-fluid" alt="Image could not load">
     </div>
-    <div class="col">
-    <img src="image/card1.webp" class="card-img-top" alt="Image could not load">
+    <div class="col grow">
+    <img src="image/LRcover.jpg" role="button" href="#" class="img-fluid" alt="Image could not load">
     </div>
-    <div class="col">
-    <img src="image/card1.webp" class="card-img-top" alt="Image could not load">
+    <div class="col grow">
+    <img src="image/GraduationCover.jpg" role="button" href="#" class="img-fluid" alt="Image could not load">
     </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col grow">
+    <img src="image/808sCover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col grow">
+    <img src="image/MBDTFcover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col grow">
+    <img src="image/WTTcover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col grow">
+    <img src="image/KWPGMCScover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col grow">
+    <img src="image/YeezusCover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col grow">
+    <img src="image/TLOPcover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col grow">
+    <img src="image/YeCover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col grow">
+    <img src="image/KSGcover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col grow">
+    <img src="image/JIKcover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col grow">
+    <img src="image/DondaCover.jpeg" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col grow">
+    <img src="image/Donda2Cover.webp" role="button" href="#" class="img-fluid" alt="Image could not load">
+    </div>
+    <div class="col"></div>
   </div>
 </div>
 
