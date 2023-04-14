7d3505f4770c29bdafbc6b44932c5d8548db04f9
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2023-04-14 20:15:59 +1200

    stuff and/or things

--- includes/nav.php
+++ includes/nav.php
@@ -1,38 +1,38 @@
-<nav class="navbar navbar-expand-lg no-padding">
-  <div class="container-fluid" style="background-color: rgba(0, 0, 0, 0.2);">
+<nav class="navbar navbar-expand-lg bg-dark no-padding z50">
+  <div class="container-fluid z50" style="background-color: rgba(0, 0, 0, 0.2);">
 
   <a href="index.php"><img src="image/logo.png" style="width:70px;height:80px;" alt="home"></a>
 
    <!-- <a class="navbar-brand" href="#">Kanye West</a>-->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler z50" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon " ></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="music.php" style="color:white">Music</a>
+    <div class="collapse navbar-collapse "  id="navbarNav">
+      <ul class="navbar-nav " >
+        <li class="nav-item " >
+          <a class="nav-link " href="music.php" style=" color:white">Music</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="color:white">Fashion</a>
+        <li class="nav-item " >
+          <a class="nav-link " href="#" style=" color:white">Fashion</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="color:white">History</a>
+        <li class="nav-item " >
+          <a class="nav-link " href="#" style=" color:white">History</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="color:white">Gallery</a>
+        <li class="nav-item " >
+          <a class="nav-link " href="#" style=" color:white">Gallery</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="color:white">Shop</a>
+        <li class="nav-item " >
+          <a class="nav-link " href="#" style=" color:white">Shop</a>
         </li>
       </ul>
     </div>
-    <div class="collapse navbar-collapse position-absolute top-25 end-0" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="font-size:30px;color:white"><i class="fas fa-shopping-cart"></i></a>
+    <div class="collapse navbar-collapse position-absolute top-25 end-0 "  id="navbarNav">
+      <ul class="navbar-nav " >
+        <li class="nav-item " >
+          <a class="nav-link " href="#" style=" font-size:30px;color:white"><i class="fas fa-shopping-cart "></i></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" style="font-size:30px;color:white"><i class="fas fa-user"></i></a>
+        <li class="nav-item " >
+          <a class="nav-link " href="#" style=" font-size:30px;color:white"><i class="fas fa-user "></i></a>
         </li>
       </ul>
     </div>
--- musictemp.php
+++ musictemp.php
@@ -6,15 +6,52 @@ include 'includes/nav.php';
 ?>
 
 <div class="container" style="max-width:100%;">
-    <div class="row">
+    <div class="row split">
         <div class="col left">
-            <br>
+        <div class="left0"></div>
+        <div id="left1 video-container">
+        <iframe id="video-player" width="560" height="315" frameborder="0" allowfullscreen></iframe>
         </div>
+
+
+        </div>
+
         <div class="col right">
-            <div class="recordcover"><img src="image/808sCover.jpeg" class="img-fluid"></div>
+            <br>
+            <div class="albumcover"><img src="image/808sCover.jpeg" class="img-fluid"></div>
+            <br>
+            <h3 class="albumtitle">808s & Heartbreak</h3>
+            <p class="albumdesp">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum soluta eius praesentium amet odio assumenda. Suscipit aut reiciendis reprehenderit ipsum mollitia doloremque, voluptatibus autem officiis accusantium id, dolorum commodi vitae!</p>
+            <p class="albumdesp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam autem eaque natus nihil doloribus repellat nam aut temporibus quod laudantium quas molestias explicabo dolores consectetur ducimus at, architecto quam? Recusandae.</p>
+            <p class="albumdesp">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor officiis unde ipsam. Amet tempora dolores quae ex, maxime assumenda aliquid, nulla perferendis a accusamus vitae! Autem quisquam sed molestiae doloremque!</p>
+            <p class="albumdesp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, provident facilis unde officiis ducimus, repellat saepe quibusdam exercitationem beatae quidem sed rerum consectetur itaque eveniet ullam harum pariatur praesentium cupiditate.</p>
+            <ul class="list-group">
+                <li class="list-group-item albumtrack">An item
+                <span class="artist">• Kanye West • Jay Z</span>
+                <span class="badge bg-secondary text-dark icon" title="Explicit">E</span>
+                <span class="badge bg-secondary text-dark icon" title="Deluxe"><span class="fas fa-star"></span></span>
+                <span class="badge bg-secondary text-dark icon" title="Interlude">I</span>
+                <span class="badge bg-secondary text-dark icon" title="Lyrics"><span class="far fa-file-alt"></span></span>
+                <span role="button"class="video-button badge bg-secondary text-dark icon" title="Play" data-video-id="sk3rpYkiHe8"><span class="fas fa-play"></span></span>
+                </li>
+                <br>
+            </ul>
         </div>
     </div>
 </div>
+
+<script>
+    const videoButtons = document.querySelectorAll('.video-button');
+    const videoPlayer = document.getElementById('video-player');
+
+    videoButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const videoId = button.getAttribute('data-video-id');
+            const videoUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+            videoPlayer.setAttribute('src', videoUrl);
+        });
+    });
+</script>
 
 <!-- Call up the footer-->
 <?php
