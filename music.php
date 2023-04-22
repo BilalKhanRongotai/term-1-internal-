c111058755c9b457059e38572c1787d1e23d7b9c
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2023-04-22 15:20:42 +1200

    Late registration

--- music.php
+++ music.php
@@ -14,7 +14,7 @@ include 'includes/nav.php';
     <a href="TCD.php"><img src="image/TCDcover.webp" class="img-fluid" alt="Image could not load"></a>
     </div>
     <div class="col grow">
-    <a href=""><img src="image/LRcover.jpg" class="img-fluid" alt="Image could not load"></a>
+    <a href="LR.php"><img src="image/LRcover.jpg" class="img-fluid" alt="Image could not load"></a>
     </div>
     <div class="col grow">
     <a href=""><img src="image/GraduationCover.jpg" class="img-fluid" alt="Image could not load"></a>
