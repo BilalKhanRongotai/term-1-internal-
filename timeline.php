d81a03ce87ac8f5f36efeb6e305306cc9ce70fb7
BilalKhanRongotai <bilal.khan@my.rongotai.school.nz>
2023-05-22 09:06:46 +1200

    n

--- timeline.php
+++ timeline.php
@@ -183,7 +183,7 @@ h4{
 
 #second{
   top: 80px;
-  background-color: gray; 
+  background-color: #555; 
 }
 
 #third {
@@ -196,16 +196,40 @@ h4{
   top: 200px;
   background-color: #555 /* Light Black */
 }
-
+.white{
+padding-top: 50px;
+padding-left: 200px;
+}
 </style>
 </head>
+<!--Timeline code-->
 <body>
+  <!--Side navigation bar to redirect to certain parts of the page -->
 <div id="mySidenav" class="sidenav" style="position:fixed">
   <a href="#Early" id="EarlyL">Early Life</a>
   <a href="#2000" id="second">2000s</a>
-  <a href="#" id="third">2010s</a>
+  <a href="#2010" id="third">2010s</a>
   <a href="#" id="fourth">2020s</a>
 </div>
+<!-- The video -->
+<div style="position: absolute; z-index: -99; width: 100%; height: 760px">
+  <iframe
+    frameborder="0"
+    height="100%"
+    width="100%"
+    src="https://www.youtube.com/embed/X3d5rT7FGLE?controls=0&amp;start=1&autoplay=1&mute=1&loop=1"
+  >
+  </iframe>
+</div>
+
+<div class="video-content">
+  <h1 class="white">Kanye West: The perfect Egoist</h1>
+  <p class="white">Kanye West's life is marked by a compelling mix of artistic brilliance, controversy, and personal struggles. Rising to fame as a successful rapper and producer, he has constantly pushed boundaries and challenged traditional norms within the music industry. Beyond his creative pursuits, Kanye has been open about his mental health battles and has made headlines for his outspoken nature and political endeavors. </p>
+  
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
 <h2>Kanye West Timeline</h2>
 
 <div class="timeline">
@@ -272,6 +296,55 @@ h4{
     <div class="content" >
     <h4><br>   <img src="Image/LRcover.jpg" style="width:35%"><b>Late Registration</b></h4>  
       <p>"Late Registration" is Kanye West's second studio album released in 2005. It features collaborations with a diverse array of artists and showcases West's evolution as a producer, incorporating live instrumentation and orchestration into his sound. The album received widespread critical acclaim for its musicality and lyrical depth, cementing West's position as one of the most innovative and boundary-pushing artists of his time.</p>
+    </div>
+  </div>
+  <div class="container left">
+    <div class="content">
+
+      <h4><br>   <img src="Image/GraduationCover.jpg" style="width:35%"><b>Graduation</b></h4>  
+    <p>"Graduation" is the third studio album by American rapper and producer Kanye West, released in 2007. The album features a more electro-pop and futuristic sound compared to his previous works, influenced by Daft Punk and other electronic music producers. With hit singles such as "Stronger" and "Good Life," the album became a commercial and critical success, praised for its innovative production and West's lyrical themes of personal growth and success. "Graduation" further established West as a major force in the music industry and solidified his reputation as a boundary-pushing artist.</p>
+      
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content" >
+    <h4><br>   <img src="Image/Gallery8.jpg" style="width:35%"><b>Donda West Death</b></h4>  
+      <p>Donda West was the mother of Kanye West and a former English professor. She passed away in November 2007, a day after receiving cosmetic surgery. Her death was a devastating loss for Kanye, who was very close to his mother and often referenced her in his music. Donda's passing also had a significant impact on Kanye's life and career, leading him to take a hiatus from music and focus on his personal well-being. Despite the tragedy, Donda's memory and legacy live on through her son's music and philanthropic efforts.</p>
+    </div>
+  </div>
+  <div class="container left">
+    <div class="content">
+
+      <h4><br>   <img src="Image/808sCover.jpeg" style="width:35%"><b>808s and Heartbreaks</b></h4>  
+    <p> "808s & Heartbreak" is Kanye West's fourth studio album released in 2008. The album features a more experimental sound, heavy use of autotune, and themes of heartbreak, loss, and loneliness. Despite mixed reviews, the album has since been recognized for its influence on popular music and cemented Kanye's reputation as a visionary artist willing to take risks. The albums dark themes are based off the personal tragedies Kanye was facing at the time which were his mother passing away and dealing a break up with his girlfriend.</p>   
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content" >
+    <h4><b>Kanye interupts Taylor Swift</b><br> 2009</h4>  
+      <p>The Taylor Swift incident happened during the 2009 MTV Video Music Awards when Kanye West interrupted Swift's acceptance speech for Best Female Video, causing a stir in the audience and backlash against West. The incident remains a significant moment in both their careers and in the history of the MTV Video Music Awards, The two have since reconciled.</p>
+    </div>
+</div>
+<div class="container left">
+    <div class="content"id="2010">
+
+      <h4><br>   <img src="Image/MBDTFcover.jpeg" style="width:25%"><b>My Beautiful Dark Twisted Fantasy</b></h4>  
+    <p> 
+"My Beautiful Dark Twisted Fantasy" is the fifth studio album by American rapper and producer Kanye West, released in 2010. The album is a sonic masterpiece, featuring lush and layered production, grandiose arrangements, and introspective lyrics exploring themes of fame, love, and self-doubt. The album was met with widespread critical acclaim and commercial success, cementing West's position as one of the most innovative and influential artists of his generation. "My Beautiful Dark Twisted Fantasy" is often regarded as one of the greatest albums of all time, and its impact can still be heard in today's music.</p>   
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content" >
+    <h4><b>Kanye West Presents: Good Music Cruel Summer,</b><br> 2012</h4>  
+      <p>"Cruel Summer" is a collaborative album by artists signed to Kanye West's GOOD Music label, released in 2012. The album features contributions from artists such as Kid Cudi, Pusha T, and Big Sean, and showcases a diverse range of musical styles. Despite mixed reviews, it remains an important piece of West's discography and a testament to his ability to curate talented artists.</p>
+    </div>
+</div>
+<div class="container left">
+    <div class="content">
+
+      <h4><br>   <img src="Image/YeezusCover.jpeg" style="width:35%"><b>        YEEZUS</b></h4>  
+    <p> 
+    "Yeezus" is the sixth studio album by Kanye West, released in 2013. The album showcases a bold and experimental sound, characterized by abrasive electronic beats, industrial influences, and aggressive lyrics. With its raw and confrontational approach, "Yeezus" challenged conventional notions of hip-hop and pushed the boundaries of artistic expression. While initially divisive among fans and critics, the album has since been recognized as a daring and influential work that solidified West's reputation as an innovative and boundary-pushing artist.</p>   
     </div>
   </div>
 </div>
