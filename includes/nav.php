f4150401a0457f743e99c52928f11b4758753062
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2023-05-24 11:39:26 +1200

    stuff

--- includes/nav.php
+++ includes/nav.php
@@ -6,12 +6,12 @@
   <a href="index.php" title="Home"><img src="image/logo.png" style="width:70px;height:80px;" alt="home"></a>
 
   <!-- Dropdown menu or when on phones -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon" ></span>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
     <!-- Page links -->
-    <div class="collapse navbar-collapse "  id="navbarNav">
-      <ul class="navbar-nav " >
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item " >
           <a class="nav-link " href="music.php" style=" color:white">Music</a>
         </li>
@@ -24,12 +24,15 @@
         <li class="nav-item " >
           <a class="nav-link " href="gallery.php" style=" color:white">Gallery</a>
         </li>
+        <!--
         <li class="nav-item " >
           <a class="nav-link " href="shop.php" style=" color:white">Shop</a>
         </li>
+  	    -->
       </ul>
     </div>
     <!-- Page links on right of nav bar -->
+    <!-- 
     <div class="collapse navbar-collapse position-absolute top-25 end-0 "  id="navbarNav">
       <ul class="navbar-nav " >
         <li class="nav-item " >
@@ -40,6 +43,7 @@
         </li>
       </ul>
     </div>
+  -->
   </div>
   
 </nav>
--- shop.php
+++ shop.php
@@ -1,9 +1,52 @@
 <!-- Call up the header and navbar and set page title-->
 <?php
-$page_title= "";
+$page_title= "Shop";
 include 'includes/header.php';
 include 'includes/nav.php';
 ?>
+
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/ShopCarousel1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <button type="button" class="btn btn-secondary">Shoes</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/ShopCarousel2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <button type="button" class="btn btn-secondary">Tops</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/ShopCarousel3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <button type="button" class="btn btn-secondary">Bottoms</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/ShopCarousel4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <button type="button" class="btn btn-secondary">Miscellaneous</button>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 <!-- Call up the footer-->
 <?php
